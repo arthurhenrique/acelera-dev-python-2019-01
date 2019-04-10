d288f55ad633c428c64f1e86651c22fc52ef8649
Arthur Henrique <arthur393@gmail.com>
2019-04-10 19:39:41 -0400

    first commit

---
+++ module01/programa02.py
@@ -0,0 +1,3 @@
+#  -*- coding: utf-8 -*-
+nome = input("qual é seu nome? ")
+print(f"Meu nome é {nome}")
---
+++ module01/programa03.py
@@ -0,0 +1,4 @@
+print("Execução direta do arquivo...")
+
+if __name__ == '__main__':
+    print("Execução indireta...")
